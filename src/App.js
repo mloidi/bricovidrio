9e0125e8df1acd9a063bcfbd59cc161c1eb9efa1
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2019-02-10 18:29:14 +0100

    remove products area

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Menu from './Components/Menu';
 import Home from './Pages/Home';
 import About from './Pages/About';
-import Products from './Pages/Products';
+// import Products from './Pages/Products';
 import Projects from './Pages/Projects';
 import Contact from './Pages/Contact';
 import NotFound from './Components/NotFound';
@@ -21,7 +21,7 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/nosotros" component={About} />
-              <Route exact path="/productos" component={Products} />
+              {/* <Route exact path="/productos" component={Products} /> */}
               <Route exact path="/trabajos" component={Projects} />
               <Route exact path="/trabajos/:id" component={Projects} />
               <Route exact path="/contacto" component={Contact} />
